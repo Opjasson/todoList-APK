0e599b5dbf51331c0a26d950e21d98ea3c53cb21
Opjasson <azmitheusos333@gmail.com>
2025-02-21 07:16:42 +0700

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Page() {
+  return (
+    <View>
+      <Text>
+        Hallo world
+      </Text>
+    </View>
+  );
+}
+
